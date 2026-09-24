ecd16d47585c757e0263e9f30b28fc4ecea433ed
manjunathnewaws-web <manjunathnewaws@gmail.com>
2026-09-24 14:31:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,7 @@
-provider "aws" {
-  region = "us-east-1"
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "my-vpc"
-  }
-}
-
-# Public Subnet
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "my-igw"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public-route-table"
-  }
-}
-
-# Associate Route Table with Public Subnet
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+resource "aws_s3_bucket" "demo" {
+  bucket = "${var.env}-jenkins-demo-${random_id.suffix.hex}"
 }
