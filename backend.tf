3c195e47ee408e700844f888753615be63731419
manjunathnewaws-web <manjunathnewaws@gmail.com>
2026-09-24 14:33:58 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "manju-tf-state-12345"
+    key          = "jenkins/infra/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
