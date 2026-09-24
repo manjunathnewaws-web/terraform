01b0b727ebf8f6cfd75163b5cdf60c64151186c2
manjunathnewaws-web <manjunathnewaws@gmail.com>
2026-09-24 15:22:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,34 @@ variable "aws_region" {
 variable "env" {
   type = string
 }
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "ssh_cidr" {
+  type        = string
+  description = "IP range allowed to SSH, e.g. 203.0.113.10/32"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Name of an existing EC2 key pair"
+  default     = null
+}
